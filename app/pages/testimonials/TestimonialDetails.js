78400e7441b616172ffc548dca843a5c026fac6d
Moizés Henriques <moizes.henriques@outlook.com>
2024-07-12 12:48:28 -0300

    Solved bug in TestimonialDetails when there's no associated action

--- app/pages/testimonials/TestimonialDetails.js
+++ app/pages/testimonials/TestimonialDetails.js
@@ -4,17 +4,21 @@
  *      This page is responsible for rendering detailed information about a
  *      single testimonial.
  * 
- *      Written by: William Soylemez
- *      Last edited: June 10, 2023 (by Moizes Almeida)
+ *      Written by: William Soylemez and Moizes Almeida
+ *      Last edited: July 12, 2024
  * 
  *****************************************************************************/
 
-
+/* Imports and set up */
 import React from "react";
 import Moment from 'moment';
-import { VStack, Image, Text, Spinner, Center } from "@gluestack-ui/themed-native-base";
+import { 
+  VStack, 
+  Text, 
+  Spinner, 
+  Center 
+} from "@gluestack-ui/themed-native-base";
 import { ScrollView, View, useWindowDimensions } from "react-native";
-
 import ActionCard from "../actions/ActionCard.js";
 import ServiceProviderCard from "../service-providers/ServiceProviderCard.js";
 import HTMLParser from "../../utils/HTMLParser.js";
@@ -23,28 +27,46 @@ import { getActionMetric } from "../../utils/common.js";
 import { connect } from "react-redux";
 import MEImage from "../../components/image/MEImage.js";
 
-function TestimonialDetails({ route, navigation, vendorsSettings, actions }) {
+function TestimonialDetails({ 
+  route, 
+  navigation, 
+  vendorsSettings, 
+  actions 
+}) {
+  /* Gets the dimensions of the user's phone */
   const { width } = useWindowDimensions();
 
+  /* Retrieves the testimonial from the route in which this page was called */
   const { testimonial_id } = route.params;
-  const [testimonial, isTestimonialLoading]
-    = useDetails("testimonials.info", { testimonial_id: testimonial_id });
 
-  const testimonialAction = actions.find(action => action.id === testimonial?.action.id);
+  /* 
+   * Uses the testimonial id to retrieve the information about the 
+   * testimonial from the API.
+   */
+  const [testimonial, isTestimonialLoading] = useDetails(
+    "testimonials.info", 
+    { testimonial_id: testimonial_id }
+  );
 
+  /* Retrieved the information about the associated action of the testiminal */
+  const testimonialAction = actions.find(
+    action => testimonial?.action && 
+    action.id === testimonial.action.id
+  );
+
+  /* Displays the testimonial information in the page */
   return (
     <View style={{ height: '100%', backgroundColor: 'white' }}>
       {
         isTestimonialLoading
           ? (
-            // Loading spinner
+            /* Loading spinner */
             <Center width="100%" height="100%">
               <Spinner size="lg" />
             </Center>
           ) : (
             <ScrollView showsVerticalScrollIndicator={false}>
               <VStack bg="white" px="3" pb="20">
-
                 {/* Image */}
                 <MEImage
                   source={{
@@ -59,7 +81,11 @@ function TestimonialDetails({ route, navigation, vendorsSettings, actions }) {
 
                 {/* Header */}
                 <Text bold fontSize="2xl" mt={3}>{testimonial.title}</Text>
-                <Text fontSize="md" color="#BAB9C0" mb={3}>By {testimonial.preferred_name} | {Moment(testimonial.created_at).format('ll')}</Text>
+                <Text fontSize="md" color="#BAB9C0" mb={3}>
+                  {/* If there's no author, display it as Anonymous */}
+                  By {testimonial.preferred_name || 'Anonymous'} | 
+                     {Moment(testimonial.created_at).format('ll')}
+                </Text>
 
                 {/* Body */}
                 <HTMLParser
@@ -71,14 +97,26 @@ function TestimonialDetails({ route, navigation, vendorsSettings, actions }) {
                   testimonialAction
                     ? (
                       <View>
-                        <Text bold fontSize="lg" mb={3} mt={5}>Associated Action</Text>
+                        <Text 
+                          bold 
+                          fontSize="lg" 
+                          mb={3} 
+                          mt={5}
+                        >
+                          Associated Action
+                        </Text>
+
                         <ActionCard
                           navigation={navigation}
                           id={testimonialAction.id}
                           title={testimonialAction.title}
                           imgUrl={testimonialAction.image?.url}
-                          impactMetric={getActionMetric(testimonialAction, "Impact")}
-                          costMetric={getActionMetric(testimonialAction, "Cost")}
+                          impactMetric={
+                            getActionMetric(testimonialAction, "Impact")
+                          }
+                          costMetric={
+                            getActionMetric(testimonialAction, "Cost")
+                          }
                         />
                       </View>
                     ) : <></>
@@ -86,17 +124,29 @@ function TestimonialDetails({ route, navigation, vendorsSettings, actions }) {
 
                 {/* Associated vendor */}
                 {
-                  (vendorsSettings.is_published && testimonial.vendor != null)
+                  (vendorsSettings.is_published && 
+                    testimonial.vendor != null)
                     ? (
                       <View>
-                        <Text bold fontSize="lg" mb={2} mt={7}>Related Vendor</Text>
+                        <Text 
+                          bold 
+                          fontSize="lg"
+                          mb={2} 
+                          mt={7}
+                        >
+                          Related Vendor
+                        </Text>
+
                         <ServiceProviderCard
                           id={testimonial.vendor.id}
                           direction="row"
                           name={testimonial.vendor.name}
-                          description="This could be a brief description of the service provider."
-                          imageURI={(testimonial.vendor.logo) ? testimonial.vendor.logo.url : null}
-                          // onPress={() => navigation.navigate("serviceProviderDetails")}
+                          description="Description of the service provider."
+                          imageURI={
+                            (testimonial.vendor.logo) 
+                              ? testimonial.vendor.logo.url 
+                              : null
+                          }
                           navigation={navigation}
                           my="2"
                         />
@@ -115,6 +165,10 @@ const textStyle = {
   fontSize: "16px",
 };
 
+/* 
+ * Transforms the local state of the app into the proprieties of the 
+ * TestimonialsDetails function, in which it is got from the API.
+ */
 const mapStateToProps = state => {
   return {
     vendorsSettings: state.vendorsPage,
